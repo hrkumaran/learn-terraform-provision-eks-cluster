96dd127ac57fe7d6a90b8d4ac11caaa2a6522ebb
Kumaran <kumaranbecse@gmail.com>
2022-08-06 12:18:27 +0530

    first commit

--- variables.tf
+++ variables.tf
@@ -1,5 +1,15 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "ap-south-1"
+}
+variable "access_key" {
+  description = "AWS access_key"
+  type        = string
+  default     = "<<>>"
+}
+variable "secret_key" {
+  description = "AWS secret_key"
+  type        = string
+  default     = "<<>>"
 }
